ccb3384cb5df13343a2387ff0cf0548fb5e2afc7
Akshay Punyapal Mehta <akshaymehta@Akshays-MacBook-Air.local>
2025-03-29 23:59:21 +0530

    job search page added

---
+++ jobs/jobs-data.js
@@ -0,0 +1,44 @@
+const jobs = [
+    {
+        index: 0,
+        icon: "../assets/google.png",
+        title: "Web Developer",
+        rate: "$900-1200/m",
+        type: "Full Time"
+    },
+    {
+        index: 1,
+        icon: "../assets/uber.png",
+        title: "Business Associate",
+        rate: "$900-1200/m",
+        type: "Part Time"
+    },
+    {
+        index: 2,
+        icon: "../assets/yahoo.png",
+        title: "Wordpress Developer",
+        rate: "$900-1200/m",
+        type: "Freelancer"
+    },
+    {
+        index: 3,
+        icon: "../assets/google.png",
+        title: "Web Developer",
+        rate: "$900-1200/m",
+        type: "Full Time"
+    },
+    {
+        index: 4,
+        icon: "../assets/uber.png",
+        title: "Business Associate",
+        rate: "$900-1200/m",
+        type: "Part Time"
+    },
+    {
+        index: 5,
+        icon: "../assets/yahoo.png",
+        title: "Wordpress Developer",
+        rate: "$900-1200/m",
+        type: "Freelancer"
+    }
+]
---
+++ jobs/jobsearch.js
@@ -0,0 +1,31 @@
+const jobList = Array.from(jobs);
+
+document.getElementById('search-input')
+    .addEventListener('keyup', (e) => {
+        const searchInput = e.target.value.toLowerCase();
+
+        const filterData = jobList.filter((item) => item.title.toLowerCase().includes(searchInput));
+        displayItem(filterData);
+    });
+
+const displayItem = (items) => {
+    const jobContainer = document.querySelector('.job-lists');
+
+    jobContainer.innerHTML = '';
+
+    items.forEach(item => {
+        const {index, icon, title, rate, type} = item;
+        const jobCardDiv = document.createElement('div');
+        jobCardDiv.className = 'job-card';
+        jobCardDiv.innerHTML = `
+            <img src="${icon}">
+            <h4>${title}</h4>
+            <p>${rate}</p>
+            <p>${type}</p>
+        `;
+
+        jobContainer.appendChild(jobCardDiv);
+    });
+};
+
+displayItem(jobList);
